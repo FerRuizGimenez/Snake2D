51c0cbdee8f6d1638b57aab7df37420271092d73
Fernando Ruiz Gimenez <48037548+FerRuizGimenez@users.noreply.github.com>
2023-01-12 23:54:24 +0100

    #4 Segments & Growing
    - Combine some of the functionality to make it so when you eat the food it will grow the snake in size
    - So the snake will have more segments being added
    - We create a list to keep track of all the segments of our snake
    - The first segment is the head of the snake
    - Create an instance of the segment prefab and then add it to the list
    - Set the position of the segment to be at the end of our snake object

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,8 +1,18 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right; //In order to keep track of our direction
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+
+    private void Start() 
+    {
+        _segments = new List<Transform>(); // Initialize the list
+        _segments.Add(this.transform); 
+
+    }
  
     private void Update() 
     {
@@ -26,10 +36,33 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate() 
     {
+        Debug.Log(_segments.Count);
+
+        for(int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );               
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment); 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Food")
+        {
+            Grow();    
+        }
     }
 }
