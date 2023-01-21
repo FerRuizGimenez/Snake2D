136689094685b76f5bed900f334e9e6f7d293f10
Fernando Ruiz Gimenez <48037548+FerRuizGimenez@users.noreply.github.com>
2023-01-21 15:03:04 +0100

    #5 Game Over
    - Final collision detection
    - Handle collision on the boundaries as well as you collide into yourself
    - Reset the state of the game (Clear out our list of segments and start over again)

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -4,31 +4,32 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right; //In order to keep track of our direction
-    private List<Transform> _segments;
+    private List<Transform> _segments = new List<Transform>();
     public Transform segmentPrefab;
+
+    public int initialSize = 4;
 
     private void Start() 
     {
-        _segments = new List<Transform>(); // Initialize the list
-        _segments.Add(this.transform); 
+        ResetState();
 
     }
  
     private void Update() 
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             _direction = Vector2.up;
         } 
-        else if(Input.GetKeyDown(KeyCode.S))
+        else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             _direction = Vector2.down;
         }   
-        else if(Input.GetKeyDown(KeyCode.A))
+        else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             _direction = Vector2.left;
         }
-        else if(Input.GetKeyDown(KeyCode.D))
+        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
             _direction = Vector2.right;
         }
@@ -36,8 +37,9 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate() 
     {
-        Debug.Log(_segments.Count);
+        //Debug.Log(_segments.Count);
 
+        // This makes sure that sort of each segment is following the one in front of it
         for(int i = _segments.Count - 1; i > 0; i--)
         {
             _segments[i].position = _segments[i - 1].position;
@@ -58,11 +60,33 @@ public class Snake : MonoBehaviour
         _segments.Add(segment); 
     }
 
+    private void ResetState()
+    {
+        for(int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }   
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for(int i = 1; i < this.initialSize; i++)
+        {
+            _segments.Add(Instantiate(this.segmentPrefab));
+        }
+
+        this.transform.position = Vector3.zero;   
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Food")
         {
             Grow();    
+        }
+        else if(other.tag == "Obstacle")
+        {
+            ResetState();
         }
     }
 }
